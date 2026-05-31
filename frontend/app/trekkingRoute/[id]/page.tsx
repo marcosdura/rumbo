e67b09ea8c01de2b0376d43b5f9eef01432b0736
Marcos Dura <marcosdurasosa01@gmail.com>
2026-05-31 12:04:48 -0300

    fixx

--- frontend/app/trekkingRoute/[id]/page.tsx
+++ frontend/app/trekkingRoute/[id]/page.tsx
@@ -1,6 +1,9 @@
+import React from "react"
 import TrekkingRouteDetails from "./TrekkingRouteDetails"
+
+const Details = TrekkingRouteDetails as React.ComponentType<{ slug?: string }>
 
 export default async function TrekkingRoutePage({ params }: { params: any }) {
   const { id } = await params
-  return <TrekkingRouteDetails slug={id} />
+  return <Details slug={id} />
 }
